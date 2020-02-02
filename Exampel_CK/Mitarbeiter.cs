5190ffba976770b0f421f12a6482fd0eb9ef5509
ImJollyHerm <60578128+ImJollyHerm@users.noreply.github.com>
2020-02-02 17:40:50 +0100

    Initial commit

---
+++ Exampel_CK/Mitarbeiter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exampel_CK
+{
+    class Mitarbeiter
+    {
+        private string name;
+        private double bruttogehalt;
+
+        public Mitarbeiter(string pName, double pBruttoGehalt)
+        {
+            name = pName;
+            bruttogehalt = pBruttoGehalt;
+        }
+
+        public void AlleMaDatenAusgeben()
+        {
+            Console.WriteLine(name + ", " +bruttogehalt+ " Euro");
+        }
+
+    }
+}
---
+++ Exampel_CK/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exampel_CK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mitarbeiter> mitarbeiter_list = new List<Mitarbeiter>();
+            mitarbeiter_list.Add(new Mitarbeiter("Hans", 1500.89));
+            mitarbeiter_list.Add(new Mitarbeiter("Max", 750.55));
+            mitarbeiter_list.Add(new Mitarbeiter("Doro", 450.00));
+            Mitarbeiter_Ausgeben(mitarbeiter_list);
+            Console.ReadLine();
+        }
+
+        private static void Mitarbeiter_Ausgeben(List<Mitarbeiter> list)
+        {
+            foreach(Mitarbeiter m in list)
+            {
+                m.AlleMaDatenAusgeben();
+            }
+        }
+    }
+}
